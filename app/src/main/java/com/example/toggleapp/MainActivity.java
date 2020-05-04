040642f67f936550bed1abe71abad82b923543d8
Janie <u1065204@hk.edu.tw>
2020-05-04 11:12:31 +0800

    123

--- app/src/main/java/com/example/toggleapp/MainActivity.java
+++ app/src/main/java/com/example/toggleapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.toggleapp;
 
 import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,33 +10,37 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
-public abstract class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-    private ImageView b_on;
-    private ImageView b_off;
-    private LinearLayout layout_bulb;
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private ImageView img_on,img_off;//宣告變數
+    private ConstraintLayout conLayout;//宣告變數
+    private ToggleButton togButton;//宣告變數
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToggleButton toggle =(ToggleButton)findViewById(R.id.toggleButton);
-        toggle.setOnCheckedChangeListener(this);
-        b_off=(ImageView)findViewById(R.id.imgoff);
-        b_on=(ImageView)findViewById(R.id.imgon);
-        b_off.setVisibility(View.GONE);
-        b_on.setVisibility(View.GONE);
-        layout_bulb=(LinearLayout)findViewById(R.id.layout);
-        layout_bulb.setBackgroundColor(Color.parseColor("#dddddd"));
+        img_on = findViewById(R.id.imageView2);//宣告變數
+        img_off = findViewById(R.id.imageView);//宣告變數
+        conLayout = findViewById(R.id.layout);//宣告變數
+        togButton = findViewById(R.id.toggleButton);//宣告變數
+
+        togButton.setOnCheckedChangeListener(this);//這個toggleButton被按下觸發
+        conLayout.setBackgroundColor(Color.parseColor("#FFFF33"));//變成黃色
+        img_off.setVisibility(View.INVISIBLE);
     }
-    protected void onCheckChanged(CompoundButton buttonView, boolean isChecked){
-        if (isChecked){
-            b_off.setVisibility(View.VISIBLE);
-            b_on.setVisibility(View.GONE);
-            layout_bulb.setBackgroundColor(Color.parseColor("#666666"));
-        }
-        else {
-            b_on.setVisibility(View.VISIBLE);
-            b_off.setVisibility(View.GONE);
-            layout_bulb.setBackgroundColor(Color.parseColor("#ffff00"));
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if(isChecked) {//如果被按下
+            conLayout.setBackgroundColor(Color.parseColor("#000000"));//顯示黑色
+            img_off.setVisibility(View.VISIBLE);//顯示
+            img_on.setVisibility(View.INVISIBLE);//消失
+
+        }else{
+            conLayout.setBackgroundColor(Color.parseColor("#FFFF33"));//顯示黃色
+            img_off.setVisibility(View.INVISIBLE);//消失
+            img_on.setVisibility(View.VISIBLE);//顯示
         }
     }
 }
